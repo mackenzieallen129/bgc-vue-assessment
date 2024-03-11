11f2c6d29d8cc48b58d5b262e02273cb9df6fd49
mackenzieallen129 <kenzieallen6@gmail.com>
2024-03-11 18:39:24 -0400

    Allow "enter" key to add item to list
    Previously, users must click the "add" button to add task to list. Now, they can use the enter key as well

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="input-group mb-3">
-    <input type="text" v-model="newTask" class="form-control" placeholder="Add a new task">
+    <input type="text" v-model="newTask" @keydown.enter="addTask" class="form-control" placeholder="Add a new task">
     <button @click="addTask" class="btn btn-primary">Add</button>
   </div>
 </template>
